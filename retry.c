cff33bee9b7e0dc402713f09551293803dca1328
minfrin <minfrin@users.noreply.github.com>
2020-01-27 16:56:16 +0200

    Update copyright header to help with Debian formatting.

--- retry.c
+++ retry.c
@@ -1,5 +1,6 @@
 /**
- *    (C) 2020 Graham Leggett
+
+ *    Copyright (C) 2020 Graham Leggett minfrin@sharp.fm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
