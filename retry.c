33c421a0f6c8738f00d577867aa17ca950b7ab3b
minfrin <minfrin@users.noreply.github.com>
2020-01-28 00:13:29 +0200

    Fix copyright line formatting.

--- retry.c
+++ retry.c
@@ -1,6 +1,5 @@
 /**
-
- *    Copyright (C) 2020 Graham Leggett minfrin@sharp.fm
+ *    Copyright (C) 2020 Graham Leggett <minfrin@sharp.fm>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
